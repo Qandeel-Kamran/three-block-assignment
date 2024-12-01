f7d14503a575a7d8707aaac7d8e9919c90bb9e67
Qandeel Kamran. <160300255+Qandeel-Kamran@users.noreply.github.com>
2024-12-01 12:42:06 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function home (){
+  return (
+    <div className="bg-gray-500 flex flex-col flex-wrap
+     md:flex-row " >
+      <div className="bg-orange-500 h-20 w-full md:w-full">Lorem1</div>
+      <div className="bg-pink-600 h-20 w-full md:w-1/2" >Lorem2</div>
+      <div className="bg-red-500 h-20 w-full md:w-1/2" >Lorem3</div>
+    </div>
+  )
+}
